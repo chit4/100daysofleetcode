dc35cd8b4378b6a4a472de696f15943dc8bba0ac
Chaitra <chit4@users.noreply.github.com>
2022-05-03 10:24:19 +0530

    Update twosum.py

--- day2/twosum.py
+++ day2/twosum.py
@@ -1,3 +1,5 @@
+#https://leetcode.com/problems/two-sum/
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         res=[[nums.index(val),nums[ind +1:].index(target-val)+ ind+1] for ind,val in enumerate(nums) if target -val in nums[ind +1:]]
